9894fa563d389cf15869a80b14073cb3ec53d6fe
Benjamin Saldman <benjaminsaldman@gmail.com>
2023-07-31 11:38:40 +0300

    Last update on project

--- index.js
+++ index.js
@@ -168,9 +168,9 @@ dashboard_socket.on('connection', async (ws) => {
                 last_event+='<br>';
                 last_event+='DEC: degrees: '+hits[i].DEC.degrees+' minutes: '+ hits[i].DEC.minutes+' seconds: '+hits[i].DEC.seconds;
                 last_event+='<br>';
-                last_event +="Time Stamp: "+new Date(hits[i].eventTS).toString();
+                last_event +="Time Stamp: "+new Date(hits[i].eventTS).toUTCString();
               }else{
-                last_event +="Time Stamp: "+new Date(hits[i].eventTS).toString();
+                last_event +="Time Stamp: "+new Date(hits[i].eventTS).toUTCString();
               }
           }
           total_events += 1;
